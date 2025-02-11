7152a57282a0a01cfd9132ad0fed944e087f9132
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-11 12:49:09 +0800

    Link to week2

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Page() {
     return (
         <div>
             <h1>Web 2 Demos</h1>
             <p>Web 2 Demos</p>
+            <p>
+                Link to week 2: <Link href="week-2">Week 2</Link>{" "}
+            </p>
         </div>
     );
 }
